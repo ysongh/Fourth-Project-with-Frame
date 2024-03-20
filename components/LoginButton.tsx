a77f08123dde6c7b00bb408360f897a70cb7f90d
You Song Hou <ysongweb3@gmail.com>
2024-03-19 22:06:26 -0400

    feat: display authentication status from Privy

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -1,15 +1,20 @@
 "use client"
 
-import {usePrivy} from '@privy-io/react-auth'
+import { usePrivy } from '@privy-io/react-auth'
 
 export default function LoginButton() {
-  const { login } = usePrivy()
+  const { login, ready, authenticated, user  } = usePrivy()
 
   return (
-    <button
-      onClick={login}
-    >
-      Log in
-    </button>
+    <>
+      {ready && !authenticated && (
+        <button
+          onClick={login}
+        >
+          Log in
+        </button>
+      )}
+      {ready && authenticated && <p>User {user?.id} is logged in.</p>}
+    </>
   )
 }
