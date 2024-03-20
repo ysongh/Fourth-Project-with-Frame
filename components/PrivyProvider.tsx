b734b5f1f54c4ff88752394f9a71682f1b506018
You Song Hou <ysongweb3@gmail.com>
2024-03-20 01:18:04 -0400

    feat: change the theme for Privy

--- components/PrivyProvider.tsx
+++ components/PrivyProvider.tsx
@@ -9,7 +9,7 @@ export default function PrivyProvider({children}: {children: React.ReactNode}) {
       config={{
         // Customize Privy's appearance in your app
         appearance: {
-          theme: 'light',
+          theme: 'dark',
           accentColor: '#676FFF',
           logo: 'https://your-logo-url',
         },
